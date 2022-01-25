5beaa85b328ce372f0c8c6aa6982e0a1f95aa5d0
sanjiv576 <83968516+sanjiv576@users.noreply.github.com>
2022-01-25 11:16:55 +0545

    Initial commit

---
+++ src/test/java/com/lcwd/EmailMessageTest.java
@@ -0,0 +1,20 @@
+package com.lcwd;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class EmailMessageTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
